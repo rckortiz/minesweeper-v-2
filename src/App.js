d76871c8f68d665bbeca26d01449e82d710ca352
Rick Ortiz <rick@Ricks-MacBook-Pro.local>
2019-11-05 10:47:51 -0500

    board displaying but needs fixing

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react'
 import Board from './components/Board'
+import BoardHead from './components/BoardHead'
 
 class App extends Component {
   state = {
     rows: 10,
     columns: 10,
     flags: 10,
-    mines: 10
+    mines: 10,
+    time: 0
   }
   render() {
     return (
       <div className="minesweeper">
+        <h1>Minesweeper</h1>
+        <BoardHead time={this.state.time} flagCount={this.state.flags} />
         <Board
           rows={this.state.rows}
           columns={this.state.columns}
